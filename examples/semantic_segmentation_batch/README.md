a9354d7175854db80f6617adf796c3761d4bd0ba
HenryYuYu [登翔] <tenghsiangyu@realtek.com.tw>
2020-03-24 19:10:48 +0800

    add script for batch converting json to gray map

---
+++ examples/semantic_segmentation_batch/README.md
@@ -0,0 +1,7 @@
+# Semantic Segmentation to Gray Map Example
+
+## Batch convert json to gray map and visualize map
+
+```bash
+labelme_json_to_dataset_bat --label-file label.txt --json-list json.txt '{shift_auto_shape_color: -2}'
+```
--- labelme/cli/json_to_dataset_bat.py
+++ labelme/cli/json_to_dataset_bat.py
@@ -25,69 +25,71 @@ def importLabel(label_file):
 
 def main():
     logger.warning('This script is aimed to convert the '
-                   'JSON file to a dataset in camvid format.')
+                   'JSON batch to gray map of DABNet format.')
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('json_file')
-    # parser.add_argument('-jl', '--json-list', default=None)
-    parser.add_argument('-lf', '--label-file', default=None)
-    parser.add_argument('-o', '--out', default=None)
+    parser.add_argument('--json-list', default=None)
+    parser.add_argument('--label-file', default=None)
     args = parser.parse_args()
 
-    json_file = args.json_file
-    json_list = args.json_list
-    label_file = args.label_file
+    # Load .json from list file
+    if not osp.isfile(args.json_list):
+        print("json_list doesn't existed!!")
+        return
+    
+    with open(args.json_list, 'r') as f:
+        json_files = f.readlines()
+    json_files = [x.strip() for x in json_files]
     
     # Import label file
-    label_name_to_value = importLabel(label_file)
+    if not osp.isfile(args.label_file):
+        print("label_file doesn't existed!!")
+        return
+    label_name_to_value = importLabel(args.label_file)
 
-    # Load .json from list file
-    # with open(json_list, 'r') as jl:
-
-
-    if args.out is None:
-        out_dir = osp.basename(json_file).replace('.', '_')
-        out_dir = osp.join(osp.dirname(json_file), out_dir)
-    else:
-        out_dir = args.out
-    if not osp.exists(out_dir):
-        os.mkdir(out_dir)
-
-    data = json.load(open(json_file))
-    imageData = data.get('imageData')
-
-    if not imageData:
-        imagePath = os.path.join(os.path.dirname(json_file), data['imagePath'])
-        with open(imagePath, 'rb') as f:
-            imageData = f.read()
-            imageData = base64.b64encode(imageData).decode('utf-8')
-    img = utils.img_b64_to_arr(imageData)
-
-    for shape in sorted(data['shapes'], key=lambda x: x['label']):
-        label_name = shape['label']
-        label_value = label_name_to_value[label_name]
+    # main loop
+    for i in range(0, len(json_files)):
+        json_file = ''.join(json_files[i])
         
-    lbl, _ = utils.shapes_to_label(
-        img.shape, data['shapes'], label_name_to_value
-    )
-    label_names = [None] * (max(label_name_to_value.values()) + 1)
-    for name, value in label_name_to_value.items():
-        label_names[value] = name
+        out_dir = json_file.split('.')[0]
+        
+        data = json.load(open(json_file))
+        imageData = data.get('imageData')
 
-    lbl_viz = imgviz.label2rgb(
-        label=lbl, img=imgviz.asgray(img), label_names=label_names, loc='rb'
-    )
+        if not imageData:
+            imagePath = os.path.join(os.path.dirname(json_file), data['imagePath'])
+            with open(imagePath, 'rb') as f:
+                imageData = f.read()
+                imageData = base64.b64encode(imageData).decode('utf-8')
+        img = utils.img_b64_to_arr(imageData)
 
-    # PIL.Image.fromarray(img).save(osp.join(out_dir, 'img.png'))
-    utils.lblsave_gray(osp.join(out_dir, 'label.png'), lbl)
-    # PIL.Image.fromarray(lbl_viz).save(osp.join(out_dir, 'label_viz.png'))
+        # check label in json is in the label file or not
+        for shape in sorted(data['shapes'], key=lambda x: x['label']):
+            label_name = shape['label']
+            if label_name in label_name_to_value:
+                label_value = label_name_to_value[label_name]
+            else:
+                label_value = len(label_name_to_value)
+                label_name_to_value[label_name] = label_value
+                print(label_name, " is not in the label file")
+        
+        lbl, _ = utils.shapes_to_label(
+            img.shape, data['shapes'], label_name_to_value
+        )
+        label_names = [None] * (max(label_name_to_value.values()) + 1)
+        for name, value in label_name_to_value.items():
+            label_names[value] = name
 
-    with open(osp.join(out_dir, 'label_names.txt'), 'w') as f:
-        for lbl_name in label_names:
-            f.write(lbl_name + '\n')
+        lbl_viz = imgviz.label2rgb(
+            label=lbl, img=imgviz.asgray(img), label_names=label_names, loc='rb'
+        )
 
-    logger.info('Saved to: {}'.format(out_dir))
+        # PIL.Image.fromarray(img).save(osp.join(out_dir, 'img.png'))
+        utils.lblsave_gray(out_dir + '.png', lbl)
+        PIL.Image.fromarray(lbl_viz).save(out_dir + '_viz.png')
 
+        logger.info('Saved to: {}'.format(out_dir))
+        # return
 
 if __name__ == '__main__':
     main()
